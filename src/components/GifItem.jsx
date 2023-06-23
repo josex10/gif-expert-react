2e0c307a4d40a9061b8a4402f470e89d95555312
Jose Manuel Badilla Porras <jose.badilla@firstfactory.com>
2023-06-23 01:54:50 -0600

    Finish the gifitem test

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export const GifItem = ({ title, url }) => {
   return (
     <div className="card">
-      <img src={url} alt="" />
+      <img src={url} alt={title} />
       <p>{title}</p>
     </div>
   );
--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,14 +1,26 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe("Testing component GifItem", () => {
   const image = {
     title: "Messi",
-    url: "https://my-messi.gif",
+    url: "https://my-messi.gif/",
   };
 
   test("Should be a match with the snapshot", () => {
     const { container } = render(<GifItem {...image} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test("Should be show the image  with the correct URL and ALT", () => {
+    render(<GifItem {...image} />);
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(image.url);
+    expect(alt).toBe(image.title);
+  });
+
+  test("Should b show the right title", () => {
+    render(<GifItem {...image} />);
+    expect(screen.getByText(image.title)).toBeTruthy();
   });
 });
